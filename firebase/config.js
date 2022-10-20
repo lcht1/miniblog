7140145096e9f2712fb65e08b96c9bab654239bd
lcht1 <manuleichtveis@gmail.com>
2022-10-20 17:22:06 -0300

    initializing firebase database

--- firebase/config.js
+++ firebase/config.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCe9IbRBXicCcRg9Z_eNdKxvbhZnpT5lUM",
     authDomain: "miniblog-6a924.firebaseapp.com",
@@ -9,3 +11,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export { db };
