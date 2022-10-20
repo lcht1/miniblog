4239782c59362fcecee956ffffaae4545a8888b5
lcht1 <manuleichtveis@gmail.com>
2022-10-20 17:17:08 -0300

    initializing firebase and cleaning up the files

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyCe9IbRBXicCcRg9Z_eNdKxvbhZnpT5lUM",
+    authDomain: "miniblog-6a924.firebaseapp.com",
+    projectId: "miniblog-6a924",
+    storageBucket: "miniblog-6a924.appspot.com",
+    messagingSenderId: "814270514603",
+    appId: "1:814270514603:web:0be0c390e3448488047eee",
+};
+
+const app = initializeApp(firebaseConfig);
