9c3015f114738fbf1cc9bf483920a4d6183a4395
lcht1 <manuleichtveis@gmail.com>
2022-10-21 11:07:11 -0300

    feat: adding routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { About } from "./pages/About/About";
+import { Home } from "./pages/Home/Home";
+
 function App() {
     return (
         <div>
-            <h1>miniblog</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
